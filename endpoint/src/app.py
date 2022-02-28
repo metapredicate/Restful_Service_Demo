9331ed405ada1b0268806bffc179476727ed0c50
Eoin Dowling <avieor1@gmail.com>
2022-02-28 16:19:18 +0000

    changes to app.py

--- endpoint/src/app.py
+++ endpoint/src/app.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_ngrok import run_with_ngrok
+from other_class import OtherClass
 
 app = Flask(__name__)
+run_with_ngrok(app)  # Start ngrok when app is run
 
 
 @app.route("/")
 def hello() -> str:
+    """
+    TODO: _summary_
+    """
     return "Hello World"
+
+@app.route("/endpoints/1/") #, methods=[POST])
+def my_func():
+    bob = ["1","2","3"]
+    return jsonify(bob)
+
+if __name__ == '__main__':
+    app.run()
